528263ba54a72449efc77f462df38aa5954db1b7
Younes El Alami <shadoow.ma@gmail.com>
2025-05-27 21:18:24 +0100

    add autocmd autotag enter

--- packages/nvim/lua/plugins/editor/init.lua
+++ packages/nvim/lua/plugins/editor/init.lua
@@ -1,27 +1,4 @@
 -- Editor plugins
-return {
-	require(
-		"plugins.editor.treesitter"
-	),
-	require(
-		"plugins.editor.tagbar"
-	),
-	require(
-		"plugins.editor.mini-modules"
-	),
-	require(
-		"plugins.editor.smart-splits"
-	),
-	require(
-		"plugins.editor.hbac"
-	),
-	require(
-		"plugins.editor.undotree"
-	),
-	require(
-		"plugins.editor.golf"
-	),
-	require(
-		"plugins.editor.autotag"
-	),
-}
+return {require("plugins.editor.treesitter"), require("plugins.editor.tagbar"), require("plugins.editor.mini-modules"),
+        require("plugins.editor.smart-splits"), require("plugins.editor.hbac"), require("plugins.editor.undotree"),
+        require("plugins.editor.golf"), require("plugins.editor.autotag")}
