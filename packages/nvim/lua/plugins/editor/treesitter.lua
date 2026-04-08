60fdf54b32a33c6606519dea00a9e5ff4ed55fb1
Younes El Alami <younes.el-alami@bruce.work>
2026-04-08 15:00:41 +0100

    reduce tree sitter context lines

--- packages/nvim/lua/plugins/editor/treesitter.lua
+++ packages/nvim/lua/plugins/editor/treesitter.lua
@@ -188,10 +188,10 @@ return {
     -- Configure treesitter context
     require('treesitter-context').setup({
       enable = true,
-      max_lines = 0,
+      max_lines = 3,
       min_window_height = 0,
       line_numbers = true,
-      multiline_threshold = 20,
+      multiline_threshold = 1,
       trim_scope = 'outer',
       mode = 'cursor',
       separator = nil,
